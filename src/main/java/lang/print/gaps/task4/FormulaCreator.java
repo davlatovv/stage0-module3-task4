098947b4150b38390abd92b34408d983782df776
davlatovv <zafargithub@gmail.com>
2023-01-09 14:51:12 +0500

    Work with variable

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task4;
 
+import java.nio.file.Files;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 1;
+        byte b = 2;
+        System.out.println((9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+        System.out.printf("%c, %c, %c, %c", c,h,a,r);
+//        System.out.println( c,h,a,r);
     }
 }
